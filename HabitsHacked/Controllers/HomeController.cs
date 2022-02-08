c1d256f3bd7448c5bcb3f7f66ee6251bc1a5260e
smurthw8 <kirsten.smurthwaite@gmail.com>
2022-02-08 07:18:12 -0700

    Update HomeController.cs
    put in potential code for controller that can be used to test group changes against, change as needed

--- HabitsHacked/Controllers/HomeController.cs
+++ HabitsHacked/Controllers/HomeController.cs
@@ -11,9 +11,12 @@ namespace HabitsHacked.Controllers
 {
     public class HomeController : Controller
     {
+        //controller to pass data to database 
+        //private HabitsContext _habitAdder { get; set; }
 
-        public HomeController()
+        public HomeController()//HabitsContext variable)
         {
+            //_habitAdder = variable;
         }
 
         public IActionResult Index()
@@ -21,10 +24,97 @@ namespace HabitsHacked.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        ////Create Task controllers
+        //[HttpGet]
+        //public IActionResult CreateTask()
+        //{
+        //    //may need to pass Categories to view for display dynamic list
+        //    ViewBag.Categories = _habitAdder.Categories.ToList();
+
+        //    return View();
+        //}
+
+        //[HttpPost]
+        ////cr = what being passed from the form
+        //public IActionResult CreateTask(ModelName cr)
+        //{
+        //    ViewBag.Categories = _habitAdder.Categories.ToList();
+
+        //    //not sure if we need validation - can delete if we don't want to use, just keep the return statement
+        //    if (ModelState.IsValid)
+        //    {
+        //        //get data from form
+        //        _habitAdder.Add(cr);
+        //        //save changes
+        //        _habitAdder.SaveChanges();
+
+        //        return RedirectToAction("Quadrant");
+        //    }
+        //    else //if data doesn't meet requirements
+        //    {
+        //        return View(cr);
+        //    }
+        //}
+
+        ////Quadrant View
+        //public IActionResult Quadrant()
+        //{
+        //    var alltasks = _habitAdder.Films
+        //        .Include(x => x.Category)
+        //        .ToList();
+
+        //    return View(alltasks);
+        //}
+
+        ////EDIT page controllers
+        //[HttpPost]
+        //public IActionResult Edit(ModelName cr)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _habitAdder.Update(cr);
+        //        _habitAdder.SaveChanges();
+
+        //        //need to redirecttoaction, or pass in all Film Collection data again
+        //        return RedirectToAction("Quadrant");
+        //    }
+        //    else
+        //    {
+        //        return View(cr);
+        //    }
+
+        //}
+
+        //[HttpGet]
+        //public IActionResult Edit(int taskid)
+        //{
+        //    ViewBag.Categories = _habitAdder.Categories.ToList();
+
+        //    //ToString pull record using Find (then do itar by ID), or Single (expects criteria to find 1 entry)
+        //    var task = _habitAdder.ModelName.Single(x => x.TaskId == taskid);
+
+        //    //let me know if want to edit in new view, or if CreateTaskView
+        //    return View("CreateTask", task);
+        //}
+
+        ////DELETE page controllers
+        //[HttpGet]
+        //public IActionResult Delete(int filmid)
+        //{
+        //    var task = _habitAdder.ModelName.Single(x => x.TaskId == taskid);
+
+        //    return View(task);
+        //}
+
+        //[HttpPost]
+        ////pass in model feilds for specific row > need to pass in Id as hidden feild on delete page
+        //public IActionResult Delete(ModelName cr)
+        //{
+        //    _habitAdder.Films.Remove(cr);
+        //    _habitAdder.SaveChanges();
+
+        //    return RedirectToAction("Quadrant");
+        //}
 
     }
 }
